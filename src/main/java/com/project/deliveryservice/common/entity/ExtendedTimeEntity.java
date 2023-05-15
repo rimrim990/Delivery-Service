094c48494416c01af10eefda8e8a8881c3d09fd9
rimrim990 <lim990718@naver.com>
2023-05-15 22:41:42 +0900

    [fix] add getter

--- src/main/java/com/project/deliveryservice/common/entity/ExtendedTimeEntity.java
+++ src/main/java/com/project/deliveryservice/common/entity/ExtendedTimeEntity.java
@@ -1,9 +1,11 @@
 package com.project.deliveryservice.common.entity;
 
 import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
 
 import java.time.LocalDateTime;
 
+@Getter
 @MappedSuperclass
 public abstract class ExtendedTimeEntity extends BaseTimeEntity {
 
