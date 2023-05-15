95c3aaa542d0d81e9c17e37462da35afbc3bed9d
rimrim990 <lim990718@naver.com>
2023-05-15 21:10:19 +0900

    [feat] add member entity

---
+++ src/main/java/com/project/deliveryservice/common/entity/Address.java
@@ -0,0 +1,13 @@
+package com.project.deliveryservice.common.entity;
+
+import jakarta.persistence.Embeddable;
+
+@Embeddable
+public class Address {
+
+    private String city;
+
+    private String street;
+
+    private String zipcode;
+}
