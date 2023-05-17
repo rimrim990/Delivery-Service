e4f5536d9d77d0ac6513be7f92baa0376f27870d
rimrim990 <lim990718@naver.com>
2023-05-17 14:33:39 +0900

    [fix] 상수를 별도의 모듈로 분리

---
+++ src/main/java/com/project/deliveryservice/common/constants/AuthConstants.java
@@ -0,0 +1,7 @@
+package com.project.deliveryservice.common.constants;
+
+public class AuthConstants {
+
+    public static final String AUTHORIZATION_HEADER = "Authorization";
+    public static final String BEARER_PREFIX = "Bearer ";
+}
