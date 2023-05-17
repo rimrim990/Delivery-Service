3bb5032e1dbdf21bda5aef46a1566de9cd040891
rimrim990 <lim990718@naver.com>
2023-05-17 16:04:05 +0900

    [feat] JWT 프로바이더 구현

--- src/main/java/com/project/deliveryservice/common/constants/AuthConstants.java
+++ src/main/java/com/project/deliveryservice/common/constants/AuthConstants.java
@@ -4,4 +4,6 @@ public class AuthConstants {
 
     public static final String AUTHORIZATION_HEADER = "Authorization";
     public static final String BEARER_PREFIX = "Bearer ";
+
+    public static final String KEY_ROLES = "roles";
 }
