a21ca9f172176d94e51efcbbcc7a0719a97b01e9
rimrim990 <lim990718@naver.com>
2023-05-18 00:38:25 +0900

    [fix] 요청 값을 바디로 받도록 수정

--- src/main/java/com/project/deliveryservice/domain/auth/controller/AuthController.java
+++ src/main/java/com/project/deliveryservice/domain/auth/controller/AuthController.java
@@ -6,10 +6,7 @@ import com.project.deliveryservice.domain.auth.service.AuthService;
 import com.project.deliveryservice.jwt.JwtTokenDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
@@ -20,7 +17,7 @@ public class AuthController {
     private final AuthService authService;
 
     @PostMapping("/login")
-    public JwtTokenDto login(LoginRequest request) {
+    public JwtTokenDto login(@RequestBody LoginRequest request) {
         return authService.login(request);
     }
 
