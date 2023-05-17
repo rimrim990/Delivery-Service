dbd5cd7a68e2633dd770ace286252a4dcb03a5be
rimrim990 <lim990718@naver.com>
2023-05-17 14:39:39 +0900

    [feat] JWT 인증 필터 구현

--- src/main/java/com/project/deliveryservice/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/project/deliveryservice/jwt/JwtAuthenticationFilter.java
@@ -1,11 +1,16 @@
 package com.project.deliveryservice.jwt;
 
+import com.project.deliveryservice.utils.JwtUtils;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
@@ -18,5 +23,19 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 
+        String jwt = JwtUtils.resolveJwtToken(request);
+
+        if (StringUtils.hasText(jwt)) {
+            try {
+                // jwt 토큰 검증 수행
+                Authentication jwtAuthenticationToken = new JwtAuthenticationToken(jwt);
+                Authentication authentication = authenticationManager.authenticate(jwtAuthenticationToken);
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            } catch (AuthenticationException authenticationException) {
+                SecurityContextHolder.clearContext();
+            }
+        }
+
+        filterChain.doFilter(request, response);
     }
 }
