30304d9a65dc7a32cb8006b98c532043eb98f6d9
rimrim990 <lim990718@naver.com>
2023-05-18 00:49:38 +0900

    [fix] token dto 에 직렬화를 위한 생성자 추가

--- src/main/java/com/project/deliveryservice/jwt/JwtTokenDto.java
+++ src/main/java/com/project/deliveryservice/jwt/JwtTokenDto.java
@@ -1,10 +1,14 @@
 package com.project.deliveryservice.jwt;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class JwtTokenDto {
 
     private String grantType;
