e4605bd5c93aaceff9d037490b3da3c1e76ce2c3
rimrim990 <lim990718@naver.com>
2023-05-17 17:45:48 +0900

    [feat] 인증 컨트롤러 구현

--- src/main/java/com/project/deliveryservice/domain/auth/controller/AuthController.java
+++ src/main/java/com/project/deliveryservice/domain/auth/controller/AuthController.java
@@ -1,17 +1,30 @@
 package com.project.deliveryservice.domain.auth.controller;
 
+import com.project.deliveryservice.domain.auth.dto.LoginRequest;
+import com.project.deliveryservice.domain.auth.service.AuthService;
+import com.project.deliveryservice.jwt.JwtDto;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
 @RequestMapping("/api/auth")
+@RequiredArgsConstructor
 public class AuthController {
 
+    private final AuthService authService;
+
     @PostMapping("/login")
-    public void login() {
-        log.warn("implement later");
+    public JwtDto login(LoginRequest request) {
+        return authService.login(request);
+    }
+
+    @PostMapping("/reissue")
+    public JwtDto reissue(@RequestHeader("AUTHORIZATION_HEADER") String bearerToken) {
+        return authService.reissue(bearerToken);
     }
 }
