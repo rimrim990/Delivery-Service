ba080c2e93e392ea6c62d58483aa4bada3c78474
rimrim990 <lim990718@naver.com>
2023-05-20 21:09:58 +0900

    [feat] Level 리포지토리 생성

--- src/main/java/com/project/deliveryservice/domain/user/repository/LevelRepository.java
+++ src/main/java/com/project/deliveryservice/domain/user/repository/LevelRepository.java
@@ -3,5 +3,9 @@ package com.project.deliveryservice.domain.user.repository;
 import com.project.deliveryservice.domain.user.entity.Level;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface LevelRepository extends JpaRepository<Level, Long> {
+
+    Optional<Level> findByName(String name);
 }
