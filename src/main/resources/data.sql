18b4b7ffda11b45a0aaea067141741ef2bd4b1ad
rimrim990 <lim990718@naver.com>
2023-05-20 21:09:10 +0900

    [feat] 기본 레벨 데이터 추가

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO LEVEL (name, policy, role)
+VALUES ('고마운분', 0, 'NORMAL'),
+       ('귀한분', 5, 'VIP'),
+       ('더귀한분', 15, 'VIP'),
+       ('천생연분', 20, 'VIP'),
+       ('관리자', -1, 'ADMIN');
