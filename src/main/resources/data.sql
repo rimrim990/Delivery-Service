e1724b678c56f2ce605ab3d8fb5a75ae4d4b6027
rimrim990 <lim990718@naver.com>
2023-05-22 16:03:58 +0900

    [fix] 테스트 유저 데이터 삽입

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -4,3 +4,6 @@ VALUES ('고마운분', 0, 'ROLE_NORMAL'),
        ('더귀한분', 15, 'ROLE_VIP'),
        ('천생연분', 20, 'ROLE_VIP'),
        ('관리자', -1, 'ROLE_ADMIN');
+
+INSERT INTO USERS (email, username, password, level_id)
+VALUES ('test@naver.com', 'test', '1234', 1);
