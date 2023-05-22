b004d5c5b47579341b76f8965e3fc6def27b1e86
rimrim990 <lim990718@naver.com>
2023-05-22 15:44:33 +0900

    [fix] ROLE 데이터 수정

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,6 @@
 INSERT INTO LEVEL (name, policy, role)
-VALUES ('고마운분', 0, 'NORMAL'),
-       ('귀한분', 5, 'VIP'),
-       ('더귀한분', 15, 'VIP'),
-       ('천생연분', 20, 'VIP'),
-       ('관리자', -1, 'ADMIN');
+VALUES ('고마운분', 0, 'ROLE_NORMAL'),
+       ('귀한분', 5, 'ROLE_VIP'),
+       ('더귀한분', 15, 'ROLE_VIP'),
+       ('천생연분', 20, 'ROLE_VIP'),
+       ('관리자', -1, 'ROLE_ADMIN');
