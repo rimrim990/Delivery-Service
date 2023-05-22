848fd998f4ce9f9ea4777fb26008bf4e0575ca4b
rimrim990 <lim990718@naver.com>
2023-05-22 17:02:26 +0900

    [feat] 회원가입 api 생성

--- src/main/java/com/project/deliveryservice/domain/auth/controller/AuthController.java
+++ src/main/java/com/project/deliveryservice/domain/auth/controller/AuthController.java
@@ -3,11 +3,15 @@ package com.project.deliveryservice.domain.auth.controller;
 import com.project.deliveryservice.common.constants.AuthConstants;
 import com.project.deliveryservice.domain.auth.dto.LoginRequest;
 import com.project.deliveryservice.domain.auth.service.AuthService;
+import com.project.deliveryservice.domain.auth.service.RegisterRequest;
+import com.project.deliveryservice.domain.user.dto.UserInfoDto;
 import com.project.deliveryservice.jwt.JwtTokenDto;
 
 import com.project.deliveryservice.utils.ApiUtils.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import static com.project.deliveryservice.utils.ApiUtils.success;
@@ -30,5 +34,11 @@ public class AuthController {
     public ApiResponse<JwtTokenDto> reissue(@RequestHeader(value = AuthConstants.AUTHORIZATION_HEADER) String bearerToken) {
         log.info("reissue requested with refreshToken " + bearerToken);
         return success(authService.reissue(bearerToken));
+    }
+
+    @PostMapping(value = "/register", produces = "application/json; charset=UTF-8")
+    public ResponseEntity<ApiResponse<UserInfoDto>> register(@RequestBody RegisterRequest request) {
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(success(authService.register(request)));
     }
 }
