f50670e1aed6a224bcba5bdade2c0430963d01c8
rimrim990 <lim990718@naver.com>
2023-05-23 17:17:41 +0900

    [test] 테스트 사용자 조회 메서드 유틸로 이동

--- src/test/java/com/project/deliveryservice/TestUtils.java
+++ src/test/java/com/project/deliveryservice/TestUtils.java
@@ -1,12 +1,62 @@
 package com.project.deliveryservice;
 
+import com.project.deliveryservice.common.entity.Address;
+import com.project.deliveryservice.domain.user.entity.Level;
+import com.project.deliveryservice.domain.user.entity.Role;
+import com.project.deliveryservice.domain.user.entity.User;
+import com.project.deliveryservice.utils.JwtUtils;
+
+import java.security.Key;
+
 public class TestUtils {
 
     public static final String testEmail = "test@naver.com";
+
+    public static final String testPassword = "1234567890";
 
     public static final String testAuthority = "ROLE_NORMAL";
 
     public static final int testExpireMin = 10;
 
     public static final int testRefreshExpireMin = 30;
+
+    /**
+     * 토큰 생성
+     * */
+    public static String getTestAccessToken(Key secretKey) {
+        return JwtUtils.createJwtToken(testEmail, testAuthority, testExpireMin, secretKey);
+    }
+
+    public static String getTestRefreshToken(Key refreshSecretKey) {
+        return JwtUtils.createJwtToken(testEmail, testAuthority, testRefreshExpireMin, refreshSecretKey);
+    }
+
+    /**
+     * 사용자 정보 조회
+     * */
+    public static User getTestUser(String email, String encodedPassword, String authority) {
+        Level level = Level.builder()
+                .role(Role.valueOf(authority))
+                .build();
+
+        return User.builder()
+                .email(email)
+                .password(encodedPassword)
+                .level(level)
+                .build();
+    }
+
+    public static User getDefaultTestUser(Long id, String email, Address address) {
+        Level level = Level.builder()
+                .name("고마운분")
+                .build();
+
+        return User.builder()
+                .id(id)
+                .email(email)
+                .address(address)
+                .level(level)
+                .build();
+    }
+
 }
--- src/test/java/com/project/deliveryservice/domain/auth/service/AuthServiceTest.java
+++ src/test/java/com/project/deliveryservice/domain/auth/service/AuthServiceTest.java
@@ -6,7 +6,6 @@ import com.project.deliveryservice.common.exception.ErrorMsg;
 import com.project.deliveryservice.domain.auth.dto.LoginRequest;
 import com.project.deliveryservice.domain.auth.dto.RegisterRequest;
 import com.project.deliveryservice.domain.user.dto.UserInfoDto;
-import com.project.deliveryservice.domain.user.entity.Role;
 import com.project.deliveryservice.domain.user.entity.Level;
 import com.project.deliveryservice.domain.user.entity.User;
 import com.project.deliveryservice.domain.user.repository.LevelRepository;
@@ -28,6 +27,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import java.util.Collections;
 import java.util.Optional;
 
+import static com.project.deliveryservice.TestUtils.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 import static org.junit.jupiter.api.Assertions.*;
@@ -45,10 +45,6 @@ class AuthServiceTest {
 
     AuthService authService;
 
-    private final String test_email = "test";
-    private final String test_password = "1234";
-    private final String test_authority = "ROLE_ADMIN";
-
     @BeforeEach
     public void setup() {
         mockUserRepository = Mockito.mock(UserRepository.class);
@@ -57,37 +53,25 @@ class AuthServiceTest {
         authService = new AuthService(mockUserRepository, mockLevelRepository, mockJwtProvider, passwordEncoder);
     }
 
-    User getUser(String email, String password, String authority) {
-        Level level = Level.builder()
-                .role(Role.valueOf(authority))
-                .build();
-
-        return User.builder()
-                .email(email)
-                .password(passwordEncoder.encode(password))
-                .level(level)
-                .build();
-    }
-
     @Test
     @DisplayName("로그인 시 사용자 이름이 존재하지 않으면 UsernameNotFoundException 던진다.")
     public void test_01() {
 
-        LoginRequest loginRequest = new LoginRequest(test_email, test_password);
+        LoginRequest loginRequest = new LoginRequest(testEmail, testPassword);
 
         Throwable throwable = assertThrows(UsernameNotFoundException.class, () -> authService.login(loginRequest));
 
-        assertThat(throwable.getMessage(), equalTo("test is not found"));
+        assertThat(throwable.getMessage(), equalTo(testEmail + " is not found"));
     }
 
     @Test
     @DisplayName("로그인 요청 시 비밀번호가 존재하지 않으면 BadCredentialsException 던진다.")
     public void test_02() {
 
-        LoginRequest loginRequest = new LoginRequest(test_email, test_password);
-        when(mockUserRepository.findByEmail(test_email)).thenReturn(
+        LoginRequest loginRequest = new LoginRequest(testEmail, testPassword);
+        when(mockUserRepository.findByEmail(testEmail)).thenReturn(
                 Optional.of(
-                        getUser(test_email, "12345", test_authority)
+                        getTestUser(testEmail, "12345", testAuthority)
                 )
         );
 
@@ -100,11 +84,11 @@ class AuthServiceTest {
     @DisplayName("유효한 로그인 요청이 들어오면 JwtTokenDto 를 반환한다.")
     public void test_03() {
 
-        LoginRequest loginRequest = new LoginRequest(test_email, test_password);
-        User user = getUser(test_email, test_password, test_authority);
-        when(mockUserRepository.findByEmail(test_email)).thenReturn(Optional.of(user));
-        when(mockJwtProvider.createAccessToken(test_email, test_authority)).thenReturn("accessToken");
-        when(mockJwtProvider.createRefreshToken(test_email, test_authority)).thenReturn("refreshToken");
+        LoginRequest loginRequest = new LoginRequest(testEmail, testPassword);
+        User user = getTestUser(testEmail, passwordEncoder.encode(testPassword), testAuthority);
+        when(mockUserRepository.findByEmail(testEmail)).thenReturn(Optional.of(user));
+        when(mockJwtProvider.createAccessToken(testEmail, testAuthority)).thenReturn("accessToken");
+        when(mockJwtProvider.createRefreshToken(testEmail, testAuthority)).thenReturn("refreshToken");
 
         JwtTokenDto jwtTokenDto = authService.login(loginRequest);
 
@@ -135,14 +119,14 @@ class AuthServiceTest {
     @DisplayName("토큰 재발급시 유효한 refreshToken 이 주어지면 JwtTokenDto 를 반환한다.")
     public void test_06() {
 
-        User user = getUser(test_email, test_password, test_authority);
-        Claims claims = Jwts.claims().setSubject(test_email);
-        claims.put(AuthConstants.KEY_ROLES, Collections.singleton(test_authority));
+        User user = getTestUser(testEmail, testPassword, testAuthority);
+        Claims claims = Jwts.claims().setSubject(testEmail);
+        claims.put(AuthConstants.KEY_ROLES, Collections.singleton(testAuthority));
 
-        when(mockUserRepository.findByEmail(test_email)).thenReturn(Optional.of(user));
+        when(mockUserRepository.findByEmail(testEmail)).thenReturn(Optional.of(user));
         when(mockJwtProvider.parseClaimsFromRefreshToken("refreshToken")).thenReturn(claims);
-        when(mockJwtProvider.createAccessToken(test_email, test_authority)).thenReturn("accessToken");
-        when(mockJwtProvider.createRefreshToken(test_email, test_authority)).thenReturn("refreshToken");
+        when(mockJwtProvider.createAccessToken(testEmail, testAuthority)).thenReturn("accessToken");
+        when(mockJwtProvider.createRefreshToken(testEmail, testAuthority)).thenReturn("refreshToken");
 
         JwtTokenDto jwtTokenDto = authService.reissue("Bearer refreshToken");
 
@@ -154,16 +138,16 @@ class AuthServiceTest {
     @Test
     @DisplayName("동일한 이메일로 회원가입을 시도하면 에러를 던진다.")
     public void test_07() {
-        User user = getUser(test_email, test_password, test_authority);
+        User user = getTestUser(testEmail, testPassword, testAuthority);
         RegisterRequest request = RegisterRequest.builder()
-                .email(test_email)
-                .password(passwordEncoder.encode(test_password))
+                .email(testEmail)
+                .password(passwordEncoder.encode(testPassword))
                 .build();
-        when(mockUserRepository.findByEmail(test_email)).thenReturn(Optional.of(user));
+        when(mockUserRepository.findByEmail(testEmail)).thenReturn(Optional.of(user));
 
         Throwable throwable = assertThrows(RuntimeException.class, () -> authService.register(request));
 
-        assertThat(throwable.getMessage(), equalTo(test_email + " already exist"));
+        assertThat(throwable.getMessage(), equalTo(testEmail + " already exist"));
     }
 
     @Test
@@ -171,27 +155,27 @@ class AuthServiceTest {
     public void test_09() {
         // given
         RegisterRequest request = RegisterRequest.builder()
-                .email(test_email)
-                .password(passwordEncoder.encode(test_password))
+                .email(testEmail)
+                .password(passwordEncoder.encode(testPassword))
                 .build();
         User user = User.builder()
                 .id(1L)
-                .email(test_email)
+                .email(testEmail)
                 .address(new Address("seoul", "songpa", "12345"))
                 .level(Level.builder().name("고마운분").build())
                 .build();
 
         // when
-        when(mockUserRepository.findByEmail(test_email)).thenReturn(Optional.empty());
+        when(mockUserRepository.findByEmail(testEmail)).thenReturn(Optional.empty());
         when(mockUserRepository.save(any())).thenReturn(user);
         UserInfoDto dto = authService.register(request);
 
         // then
-        verify(mockUserRepository, times(1)).findByEmail(test_email);
+        verify(mockUserRepository, times(1)).findByEmail(testEmail);
 
         assertThat(dto.getId(), equalTo(1L));
         assertThat(dto.getLevel(), equalTo("고마운분"));
         assertThat(dto.getAddress(), equalTo("seoul songpa 12345"));
-        assertThat(dto.getEmail(), equalTo(test_email));
+        assertThat(dto.getEmail(), equalTo(testEmail));
     }
 }
